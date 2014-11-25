a17103029acebd542c65ac7c42a2c456872209a5
Tonny <tonny@mycompcity.com>
2014-11-24 20:02:19 -0600

    Added a header redirect to contact-thanks page after an email has been send

--- TreeHouse/PHP Development/shirts4mike/contact-process.php
+++ TreeHouse/PHP Development/shirts4mike/contact-process.php
@@ -1,6 +1,12 @@
 <?php
-var_dump($_REQUEST);
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
+$email_body = "";
+$email_body = $email_body . "Name: " . $name . "\n";
+$email_body = $email_body . "Email: " . $email . "\n";
+$email_body = $email_body . "Message: " . $message;
+
+// TODO Send Email
+header("Location: contact-thanks.php")
 ?>
